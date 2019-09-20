f3d0499c677f8a5706d1bc3f38c8af682e872f62
Edward Muturi <kakaturi300@gmail.com>
2019-09-20 22:36:34 +0300

    allow a maximum of 10 parameters in a constructor a file with bigger columns than that is not supported

--- app/src/main/java/com/edward/csvprocessor/util/Util.java
+++ app/src/main/java/com/edward/csvprocessor/util/Util.java
@@ -28,6 +28,7 @@ public class Util {
     /**
      * open file manager
      * allow selection of any text file
+     *
      * @return Intent
      */
     public static Intent openFileIntent() {
@@ -38,8 +39,7 @@ public class Util {
     }
 
     /**
-     * @param context
-     * get file path from given uri  {@param fileUri}
+     * @param context get file path from given uri  {@param fileUri}
      * @return String
      */
     public static String getPathFromURI(Context context, Uri fileUri) {
@@ -56,6 +56,7 @@ public class Util {
 
     /**
      * locate and read file from {@param path}
+     *
      * @return String
      */
     public static List<Cities> readCSVFile(Context context, String path, String splitter) {
@@ -67,35 +68,69 @@ public class Util {
             FileReader fileReader = new FileReader(path);
             //wrap file reader in BufferedReader
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            Cities cities;
+            Cities cities = null;
 
             //get titles in first line
             String[] titles = bufferedReader.readLine().split(splitter);
 
 
-                //read and store remaining data; body
+            //read and store remaining data; body
             while ((line = bufferedReader.readLine()) != null) {
                 String[] values = line.split(splitter);
 
+                if (values.length == 1) {
+                    cities = new Cities(values[0]);
 
-                cities = new Cities(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
+                } else if (values.length == 2)
+                    cities = new Cities(values[0], values[1]);
+
+                else if (values.length == 3)
+                    cities = new Cities(values[0], values[1], values[2]);
+
+                else if (values.length == 4)
+                    cities = new Cities(values[0], values[1], values[2], values[3]);
+
+                else if (values.length == 5)
+                    cities = new Cities(values[0], values[1], values[2], values[3], values[4]);
+                else if (values.length == 6)
+                    cities = new Cities(values[0], values[1], values[2], values[3], values[4], values[5]);
+
+                else if (values.length == 7)
+                    cities = new Cities(values[0], values[1], values[2], values[3], values[4], values[5], values[6]);
+
+                else if (values.length == 8)
+                    cities = new Cities(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+
+                else if (values.length == 9)
+                    cities = new Cities(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8]);
+
+                else if (values.length == 10)
+                    cities = new Cities(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
+                else
+                    Toast.makeText(context, "Error reading file chosen file, choose a different file", Toast.LENGTH_LONG).show();
+
+
                 citiesList.add(cities);
 
+            }
+
+            for (Cities city : citiesList) {
+                System.out.println("Some value" + city);
             }
 
             bufferedReader.close(); //close file
 
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("Unable to open file " + path);
-            Toast.makeText(context, "Unable to open file, choose a different file", Toast.LENGTH_LONG ).show();
+            Toast.makeText(context, "Unable to open file, choose a different file", Toast.LENGTH_LONG).show();
 
         } catch (IOException ioException) {
             System.out.println("Error reading file " + path);
-            Toast.makeText(context, "Error reading file chosen file, choose a different file", Toast.LENGTH_LONG ).show();
+            Toast.makeText(context, "Error reading file chosen file, choose a different file", Toast.LENGTH_LONG).show();
 
-        } catch (ArrayIndexOutOfBoundsException indexOutOfBoundException){
+        } catch (ArrayIndexOutOfBoundsException indexOutOfBoundException) {
             System.out.println("Error reading file " + indexOutOfBoundException);
-            Toast.makeText(context, "Cannot read this file, choose a different file", Toast.LENGTH_LONG ).show();
+            Toast.makeText(context, "Cannot read this file, choose a different file", Toast.LENGTH_LONG).show();
 
         }
         return citiesList;
