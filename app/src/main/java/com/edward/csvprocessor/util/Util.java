1d103f89e4d74c1611af77661925895524db41bc
Edward Muturi <kakaturi300@gmail.com>
2019-09-20 19:38:58 +0300

    changed something, added an empty line

--- app/src/main/java/com/edward/csvprocessor/util/Util.java
+++ app/src/main/java/com/edward/csvprocessor/util/Util.java
@@ -62,6 +62,7 @@ public class Util {
             while ((line = bufferedReader.readLine()) != null) {
                 //split data by comma ,
                 String[] values = line.split(",");
+
                 Cities cities = new Cities(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
                 citiesList.add(cities);
 
